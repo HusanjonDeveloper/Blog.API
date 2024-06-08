de8c8be75d6202f94da75c05cd0233cf4a7623e6
HusanjonDeveloper <120047912+HusanjonDeveloper@users.noreply.github.com>
2024-06-08 15:51:57 +0500

    jhjh

--- Blog_Client/Program.cs
+++ Blog_Client/Program.cs
@@ -1,4 +1,4 @@
-using Blog_Client.Shared;
+using Blog_Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44349/") });
 
 await builder.Build().RunAsync();
  
